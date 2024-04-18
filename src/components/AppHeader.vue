ff5ae98304be8b6291d8afcda2b299b90e2f2578
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-18 20:12:06 +0200

    add component with structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <nav class="navbar bg-body-tertiary container">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Boolfolio
+            </a>
+        </div>
+    </nav>
+</template>
+
+<style>
+
+</style>
