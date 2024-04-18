f9f2fd4382dc61c88517c2121fd869d2ddfeb2de
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-18 20:26:12 +0200

    add structure

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,63 @@
+<script>
+
+import axios from 'axios';
+import { api, store } from '../store';
+
+export default {
+    data() {
+        return {
+            store,
+
+            title: 'Projects',
+
+        }
+
+    },
+
+    created() {
+
+        axios.get(api.rootUrl + 'projects').then((response) => {
+
+            console.log(response.data.data);
+
+            store.projects = response.data.data;
+
+        });
+
+    },
+
+
+}
+</script>
+
+<template>
+
+    <div>
+        <table class="table">
+            <thead>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Content</th>
+            </thead>
+
+            <tbody>
+                <tr v-for="proj in store.projects">
+                    <td>
+                        {{ proj.id }}
+                    </td>
+
+                    <td>
+                        {{ proj.title }}
+                    </td>
+
+                    <td>
+                        {{ proj.content }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+
+<style lang="scss" scoped></style>
