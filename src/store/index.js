b057c612871f4b3b57e6221cae0689c5fe7c9d11
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-18 20:03:08 +0200

    add items to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { reactive } from "vue";
 
-
+export const api = {
+    rootUrl: 'http://127.0.0.1:8000/api/',
+};
 
 
 // reactive itmes
 export const store = reactive({
+
+    projects: [],
 
 });
