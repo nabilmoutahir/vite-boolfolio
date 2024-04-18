be6307046cf364eaaa7870405096e537cf9375d9
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-18 18:48:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+    data() {
+      return {
+
+        title: 'hello',
+
+
+      }
+
+
+    }
+};
+
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+
+  @use './scss/general.scss' as *;
+
+</style>
+
